d1f3860c4c3df5e27e373b342140092078ec29fc
s2847547 <s2847547@students.fscj.edu>
2025-10-02 15:27:35 -0400

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Corey Burgos on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
